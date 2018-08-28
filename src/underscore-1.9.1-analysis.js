4dad44d6e928b669fc9ed19955852e58a44317a6
chao-hua <chaohua@outlook.com>
2018-08-28 17:19:43 +0800

    update

--- src/underscore-1.9.1-analysis.js
+++ src/underscore-1.9.1-analysis.js
@@ -1208,6 +1208,7 @@
   // be triggered. The function will be called after it stops being called for
   // N milliseconds. If `immediate` is passed, trigger the function on the
   // leading edge, instead of the trailing.
+  // TODO
   _.debounce = function(func, wait, immediate) {
     var timeout, result;
 
